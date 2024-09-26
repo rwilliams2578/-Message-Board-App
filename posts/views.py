130075be1a55933dd1dcb3762c869c850dcf4873
Revelle Williams <rwilliams2578@mail.kvcc.edu>
2024-09-26 19:34:02 -0400

    Add post url, view, and template.

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Post
 
-# Create your views here.
+
+class HomePageView(ListView):
+    """Home Page View"""
+
+    # After defining what model to use here,
+    # the ListView we are inheriting from will
+    # automatically fetch out all posts from the DB
+    # and store that in a variable called post_list
+    # that is then passed to the template.
+    # we can't see this magic!
+    # The "formula" is always <model_name>_list
+    # where <model_name> is the name of the model
+    # converted to lower case.
+    model = Post
+    template_name = "home.html"
