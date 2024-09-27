730fdb4b9a1761b88c37bf70afe6e1d2757a6ea6
Revelle Williams <rwilliams2578@mail.kvcc.edu>
2024-09-26 20:28:34 -0400

    modify posts/tests.py

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,38 @@
 from django.test import TestCase
 
-# Create your tests here.
+from django.urls import reverse
+
+from .models import Post
+
+
+class PostTests(TestCase):
+    """Post Tests"""
+
+    @classmethod
+    def setUpTestData(cls):
+        """Set up test data for all tests"""
+        cls.post = Post.objects.create(text="This is a test!")
+
+    def test_model_content(self):
+        """Test the model content"""
+        self.assertEqual(self.post.text, "This is a test!")
+
+    def test_url_exists_at_correct_location(self):
+        """Test that url exist at the correct location"""
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_available_by_name(self):
+        """Test url is available by name"""
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_template_name_correct(self):
+        """Test that the template name is correct"""
+        response = self.client.get(reverse("home"))
+        self.assertTemplateUsed(response, "home.html")
+
+    def test_template_content(self):
+        """Test the content of the template"""
+        response = self.client.get(reverse("home"))
+        self.assertContains(response, "This is a test!")
